c336c0856b4621748a57a9415b4252e92eee323f
AyhamD <ayhamdarwish1993@gmail.com>
2023-12-01 15:02:25 +0100

    add pagination to the table

--- src/shared/components/table.tsx
+++ src/shared/components/table.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Helicopter from "../../components/Helicopters/Helicopter";
 
 interface TableProps {
@@ -8,6 +8,23 @@ interface TableProps {
 }
 
 const Table = ({ headers, body, column = headers }: TableProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+
+  const paginate = (data) => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return data.slice(startIndex, endIndex);
+  };
+
+  const paginatedBody = paginate(body);
+
+  const totalPages = Math.ceil(body.length / itemsPerPage);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className="table-responsive">
       <table className="table">
@@ -19,19 +36,33 @@ const Table = ({ headers, body, column = headers }: TableProps) => {
           </tr>
         </thead>
         <tbody>
-          {body.length > 0
-            ? body.map((row, rowIndex) => (
+          {paginatedBody.length > 0
+            ? paginatedBody.map((row, rowIndex) => (
                 <tr key={rowIndex}>
-                  {column.map((header, colIndex) => (
-                    <td key={colIndex}>
-                      {row[header] !== "" ? row[header] : "No value"}
-                    </td>
-                  ))}
+                  {column
+                    ? column.map((header, colIndex) => (
+                        <td key={colIndex}>
+                          {row[header] !== "" ? row[header] : "No value"}
+                        </td>
+                      ))
+                    : headers.map((header, colIndex) => (
+                        <td key={colIndex}>
+                          {row[header] !== "" ? row[header] : "No value"}
+                        </td>
+                      ))}
                 </tr>
               ))
             : "the file are empty"}
         </tbody>
       </table>
+      <div>
+        {body.length > itemsPerPage &&
+          Array.from({ length: totalPages }, (_, index) => (
+            <button key={index} onClick={() => handlePageChange(index + 1)}>
+              {index + 1}
+            </button>
+          ))}
+      </div>
     </div>
   );
 };
